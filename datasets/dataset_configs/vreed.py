fee2630489732a350b9ef1423f29ec0a349d0b5f
roysup <roysu@deakin.edu.au>
2026-03-14 00:54:40 +0000

    init

--- datasets/dataset_configs/vreed.py
+++ datasets/dataset_configs/vreed.py
@@ -54,5 +54,5 @@ def load_vreed_df(preserve_trial_order: bool = False) -> pd.DataFrame:
         for id_trial in unique_id_trials:
             reordered = pd.concat([reordered, df[df['ID_video'] == id_trial]])
         df = reordered.reset_index(drop=True)
-
+    
     return df
