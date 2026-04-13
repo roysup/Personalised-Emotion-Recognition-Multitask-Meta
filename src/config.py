64270de6390ac4596c789d6974c54aa3ed001df5
roysup <roysu@deakin.edu.au>
2026-04-14 09:05:54 +1000

    sync

--- src/config.py
+++ src/config.py
@@ -286,10 +286,10 @@ _DATASET_REGISTRY = {
         'trial_col':      'Trial',
         'uw_logvar_lr_ar': 4e-3,
         'uw_logvar_lr_va': 1e-3,
-        'transfer_mtl_lr_pt_ar': 3e-4,
-    'transfer_mtl_lr_pt_va': 3e-4,
-    'transfer_mtl_lr_ft_ar': 3e-4,
-    'transfer_mtl_lr_ft_va': 3e-4,
+        'transfer_mtl_lr_pt_ar': 3e-3, #3e-4,
+        'transfer_mtl_lr_pt_va': 3e-4,
+        'transfer_mtl_lr_ft_ar': 3e-3, #3e-4,
+        'transfer_mtl_lr_ft_va': 3e-4,
     },
 }
 
